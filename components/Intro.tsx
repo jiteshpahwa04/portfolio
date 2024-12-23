b17cf84390c285ab91488d49fc0fbee401c36089
Jitesh Pahwa <jiteshofficial2004@gmail.com>
2024-12-23 10:29:04 +0530

    Updated Portfolio Link

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -119,7 +119,7 @@ export default function Intro() {
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           // onClick={downloadResume}
-          href="https://drive.google.com/file/d/1q0Uu1X5lO1a7Z6xWnQw8U46F8Uj6KWUN/view?usp=drive_link"
+          href="https://drive.google.com/file/d/1TGu7-UqPoHmverVJa0yQtv9IMLCgt1nw/view"
           target="_blank"
           download
         >
